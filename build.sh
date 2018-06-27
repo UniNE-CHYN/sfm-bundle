cc6c0c0675f4ef48a686d94fe870fde70f32cd5f
Laurent FASNACHT <l@libres.ch>
2018-06-27 15:25:38 +0200

    Build script update

--- build.sh
+++ build.sh
@@ -1,25 +1,23 @@
 #!/usr/bin/env bash
 
 
-TARGET=$(dirname "$0")
+TARGET="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "Installing to folder: $TARGET (ctrl-c within 5s to abort!)"
 sleep 5
 
-cd $TARGET
-cd src
+cd $TARGET/src
 cd colmap
-mkdir build
+mkdir -p build
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=$TARGET
 make && make install
-cd ../..
 
-mkdir eigen_build && cd eigen_build
+cd $TARGET/src
+mkdir -p eigen_build && cd eigen_build
 cmake . ../eigen  -DCMAKE_INSTALL_PREFIX=$TARGET
 make && make install
-cd ..
 
-mkdir openMVS_build && cd openMVS_build
-cmake . ../openMVS -DCMAKE_INSTALL_PREFIX=$TARGET -DCMAKE_BUILD_TYPE=Release -DVCG_DIR="$TARGET/src/vcglib" -DEIGEN_DIR=$TARGET/include/eigen3
+cd $TARGET/src
+mkdir -p openMVS_build && cd openMVS_build
+cmake . ../openMVS -DCMAKE_INSTALL_PREFIX=$TARGET -DCMAKE_BUILD_TYPE=Release -DVCG_DIR="$TARGET/src/VCG" -DEIGEN_DIR=$TARGET/include/eigen3
 make && make install
-cd ..
